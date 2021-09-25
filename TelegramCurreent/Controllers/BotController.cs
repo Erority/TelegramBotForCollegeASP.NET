7a53338baa19984b682d6ee0e7224f9e1da72cdd
Denis Bolshachkov <bolshachckov@ya.ru>
2021-09-25 20:58:45 +0300

    init commit

---
+++ TelegramCurreent/Controllers/BotController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramCurreent.Controllers
+{
+    [ApiController]
+    [Route("api/bot")]
+    public class BotController : ControllerBase
+    {
+        public async Task<IActionResult> Post([FromBody] Update update)
+        {
+            TelegramBotClient client = new TelegramBotClient("2003705144:AAFIB8or5Pl7yQa_R0yy3s5t3ntWGULnisA");
+
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await client.SendTextMessageAsync(update.Message.From.Id, "answer");
+            }
+
+
+            return Ok();
+        }
+    }
+}
